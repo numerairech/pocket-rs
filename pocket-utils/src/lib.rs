0988a5d6550a35d7aef01e3d81b05b24353c3c95
Enrique Ortiz <hi@enriqueortiz.dev>
2022-03-01 12:45:28 -0400

    feat(utils): custom & proper errors for utility functions (#2)

--- pocket-utils/src/lib.rs
+++ pocket-utils/src/lib.rs
@@ -1,20 +1,46 @@
 use sha2::{Digest, Sha256};
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error("Invalid private key length")]
+    InvalidPrivateKeyLength,
+    #[error("Invalid public key length")]
+    InvalidPublicKeyLength,
+    #[error("Invalid public key format (expected hex)")]
+    InvalidPublicKeyFormat,
+}
 
 /// Computes the address of an ed25519 public key.
-pub fn address_from_public_key(public_key: String) -> String {
-    let result = Sha256::digest(hex::decode(public_key).expect("Invalid public key"));
+pub fn address_from_public_key(public_key: String) -> Result<String, Error> {
+    let decoded_public_key = hex::decode(public_key);
+    let decoded_public_key = match decoded_public_key {
+        Ok(pk) => pk,
+        Err(e) => match e {
+            hex::FromHexError::OddLength => return Err(Error::InvalidPublicKeyLength),
+            hex::FromHexError::InvalidStringLength => return Err(Error::InvalidPublicKeyLength),
+            hex::FromHexError::InvalidHexCharacter { .. } => {
+                return Err(Error::InvalidPublicKeyFormat)
+            }
+        },
+    };
+
+    let result = Sha256::digest(decoded_public_key);
     let mut buf = [0u8; 64];
 
     let address = base16ct::lower::encode_str(&result, &mut buf).unwrap();
 
-    address.to_string().chars().take(40).collect()
+    Ok(address.to_string().chars().take(40).collect())
 }
 
 /// Extracts the public key from a 64-byte long ed25519 private key.
 ///
 /// The public key from a ed25519 private key will always be the last 64 characters.
-pub fn public_key_from_private(private_key: String) -> String {
-    private_key.chars().skip(64).collect()
+pub fn public_key_from_private(private_key: String) -> Result<String, Error> {
+    match private_key.len() {
+        128 => Ok(private_key.chars().skip(64).collect()),
+        _ => return Err(Error::InvalidPrivateKeyLength),
+    }
 }
 
 #[cfg(test)]
@@ -26,7 +52,14 @@ mod tests {
         let private_key: String = "1f8cbde30ef5a9db0a5a9d5eb40536fc9defc318b8581d543808b7504e0902bcb243b27bc9fbe5580457a46370ae5f03a6f6753633e51efdaf2cf534fdc26cc3".to_string();
         let public_key: String =
             "b243b27bc9fbe5580457a46370ae5f03a6f6753633e51efdaf2cf534fdc26cc3".to_string();
-        assert_eq!(public_key_from_private(private_key), public_key);
+        assert_eq!(public_key_from_private(private_key).unwrap(), public_key);
+    }
+
+    #[test]
+    fn errors_with_invalid_len() {
+        let private_key: String =
+            "b243b27bc9fbe5580457a46370ae5f03a6f6753633e51efdaf2cf534fdc26cc3".to_string(); // First 64 characters removed
+        assert!(public_key_from_private(private_key).is_err());
     }
 
     #[test]
@@ -34,6 +67,6 @@ mod tests {
         let public_key: String =
             "b243b27bc9fbe5580457a46370ae5f03a6f6753633e51efdaf2cf534fdc26cc3".to_string();
         let address = "b50a6e20d3733fb89631ae32385b3c85c533c560".to_string();
-        assert_eq!(address_from_public_key(public_key), address);
+        assert_eq!(address_from_public_key(public_key).unwrap(), address);
     }
 }
