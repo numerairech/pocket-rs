0d1167a0f3531ca15399ec9c5743da036302f686
Enrique Ortiz <hi@enriqueortiz.dev>
2022-02-28 23:29:30 -0400

    chore(utils): add public_key_from_private with tests

--- pocket-utils/src/lib.rs
+++ pocket-utils/src/lib.rs
@@ -1,18 +1,16 @@
-pub fn gm() {
-    println!("gm");
-}
-
-pub fn get_gm() -> &'static str {
-    "gm"
+pub fn public_key_from_private(private_key: String) -> String {
+    private_key.chars().skip(64).collect()
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::get_gm;
+    use crate::public_key_from_private;
 
     #[test]
-    fn it_gets_gm() {
-        let gm = get_gm();
-        assert_eq!(gm, "gm");
+    fn can_get_public_key() {
+        let private_key: String = "1f8cbde30ef5a9db0a5a9d5eb40536fc9defc318b8581d543808b7504e0902bcb243b27bc9fbe5580457a46370ae5f03a6f6753633e51efdaf2cf534fdc26cc3".to_string();
+        let public_key: String =
+            "b243b27bc9fbe5580457a46370ae5f03a6f6753633e51efdaf2cf534fdc26cc3".to_string();
+        assert_eq!(public_key_from_private(private_key), public_key);
     }
 }
